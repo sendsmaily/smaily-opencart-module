0c44789c93a3c903c1d3a5763ba24afa54ceefc4
Kaarel <kaarel.rooparg@gmail.com>
2019-06-05 13:01:38 +0300

    Add php5.6 support for version 2.2.0

--- opencart 2.2.0.0/upload/admin/controller/module/smaily_for_opencart.php
+++ opencart 2.2.0.0/upload/admin/controller/module/smaily_for_opencart.php
@@ -11,7 +11,7 @@
  *
  * Plugin Name: Smaily for Opencart
  * Description: Smaily email marketing and automation extension plugin for Opencart.
- * Version: 1.1.1
+ * Version: 1.1.2
  * License: GPL3
  * Author: Smaily
  * Author URI: https://smaily.com/
@@ -285,7 +285,7 @@ class ControllerModuleSmailyForOpencart extends Controller {
                 $data['sync_token'] = $this->config->get('smaily_for_opencart_sync_token');
             } else {
                 // Generate random token if not saved in db.
-                $data['sync_token'] = bin2hex(random_bytes(6));
+                $data['sync_token'] = bin2hex(openssl_random_pseudo_bytes(6));
             }
         }
 
@@ -316,7 +316,7 @@ class ControllerModuleSmailyForOpencart extends Controller {
                 $data['cart_token'] = $this->config->get('smaily_for_opencart_cart_token');
             } else {
                 // Generate random token if not saved in db.
-                $data['cart_token'] = bin2hex(random_bytes(6));
+                $data['cart_token'] = bin2hex(openssl_random_pseudo_bytes(6));
             }
         }
 
@@ -441,7 +441,7 @@ class ControllerModuleSmailyForOpencart extends Controller {
      * @param string $password
      * @return void
      */
-    public function validateSmailyCredentials(string $subdomain, string $username, string $password) {
+    public function validateSmailyCredentials($subdomain, $username, $password) {
         // Response.
         $response = [];
         // cUrl
