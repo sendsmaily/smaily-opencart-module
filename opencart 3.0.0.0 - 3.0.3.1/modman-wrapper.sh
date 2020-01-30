a730ee62ab273d839638c99eaca1259ed217fd7d
TomAbel <tomkristian.abel@tptlive.ee>
2020-01-30 11:53:03 +0200

    remove todo

--- opencart 3.0.0.0 - 3.0.3.1/modman-wrapper.sh
+++ opencart 3.0.0.0 - 3.0.3.1/modman-wrapper.sh
@@ -3,6 +3,5 @@
 if [ ! -d $MAGE_ROOT_DIR/.modman ] ; then
     cd $MAGE_ROOT_DIR && modman init
 fi
-# TODO: List all files in folder and create modman file based on that
 modman link /var/www/html/smailyfiles
 docker-php-entrypoint "$@"
