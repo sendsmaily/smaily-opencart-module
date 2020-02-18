1c62d6129d7b73185e278c5df18887c8182da678
Kaarel <kaarel.rooparg@gmail.com>
2020-02-18 15:08:59 +0200

    Remove upgrade functionality

--- opencart 2.2.0.0/upload/admin/model/smailyforopencart/admin.php
+++ opencart 2.2.0.0/upload/admin/model/smailyforopencart/admin.php
@@ -18,57 +18,8 @@ class ModelSmailyForOpencartAdmin extends Model {
         );
     }
 
-
-    public function upgrade() {
-        // Load models.
-        $this->load->model('extension/modification');
-        $this->load->model('setting/setting');
-        // Module modification settings.
-        $smaily_module = $this->model_extension_modification->getModificationByCode('smaily_for_opencart_extension');
-        
-        // Stop if module hasn't been installed with adding it as a modification.
-        if (empty($smaily_module)) {
-            return;
-        }
-
-        $version = $smaily_module['version'];
-
-        // Version 1.2.0 - standardize abandoned cart fields.
-        if (version_compare($version, '1.2.0', '=')) {
-            $cart_additional = $this->config->get('smaily_for_opencart_abandoned_additional');
-
-            // Add fist_name and last_name if abandoned cart was active. Previous default values.
-            $enabled = $this->config->get('smaily_for_opencart_enable_abandoned');
-            if ((int) $enabled === 1) {
-                // If there was no additional fields before add defaults.
-                if (! $cart_additional) {
-                    $this->model_setting_setting->editSettingValue(
-                        'smaily_for_opencart',
-                        'smaily_for_opencart_abandoned_additional',
-                        ['first_name', 'last_name']
-                    );
-                } else {
-                    // Add new fields to the list.
-                    if (!in_array('first_name', $cart_additional)) {
-                        array_push($cart_additional, 'first_name');
-                    }
-
-                    if (!in_array('last_name', $cart_additional)) {
-                        array_push($cart_additional, 'last_name');
-                    }
-
-                    $this->model_setting_setting->editSettingValue(
-                        'smaily_for_opencart',
-                        'smaily_for_opencart_abandoned_additional',
-                        $cart_additional
-                    );
-                }
-            }
-        }
-    }
-
     /**
-     * Remove smaily_abandoned_carts table.
+     * Remove smaily_abandoned_carts table and saved settings.
      *
      * @return void
      */
