e4af8c6576cc17762b54096cfb0fd0ed76fe793e
Kaarel <kaarel.rooparg@gmail.com>
2020-07-08 11:55:01 +0300

    Add abandoned init time when activating sync

--- upload/admin/controller/extension/module/smaily_for_opencart.php
+++ upload/admin/controller/extension/module/smaily_for_opencart.php
@@ -54,6 +54,19 @@ class ControllerExtensionModuleSmailyForOpencart extends Controller {
             if ($validated) {
                 $data['smaily_for_opencart_validated'] = $validated;
             }
+
+            // Abandoned cart start time.
+            $abandoned_cart_enabled = (int) $this->model_setting_setting->getSettingValue(
+                'smaily_for_opencart_enable_abandoned'
+            );
+            $data['smaily_for_opencart_abandoned_cart_time'] = $this->model_setting_setting->getSettingValue(
+                'smaily_for_opencart_abandoned_cart_time'
+            );
+            // Update start time only if abandoned cart was previously disabled and now being activated.
+            if (! $abandoned_cart_enabled && (int) $data['smaily_for_opencart_enable_abandoned']) {
+                $data['smaily_for_opencart_abandoned_cart_time'] = date('Y-m-d H:i:s');
+            }
+
             // Add sync time.
             $sync_time = $this->model_setting_setting->getSettingValue('smaily_for_opencart_sync_time');
             // Add init time before sync.
