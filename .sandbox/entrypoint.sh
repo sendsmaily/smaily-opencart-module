73f9975598cc384e7801685ae18e27ce7f0fd9b2
Kait Todesk <kaittodesk@gmail.com>
2020-11-19 17:20:10 +0200

    Simplify development environment management

--- .sandbox/entrypoint.sh
+++ .sandbox/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Wait for MySQL to start.
 mysql_ready() {
-    mysqladmin ping --host=database --user=root --password=smailydev1 > /dev/null 2>&1
+    mysqladmin ping --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWORD > /dev/null 2>&1
 }
 while !(mysql_ready); do
     sleep 1
@@ -16,27 +16,25 @@ if [ -d install ]; then
 
     # Install OpenCart through the CLI installer.
     php ./install/cli_install.php install \
-        --db_host database \
-        --db_user root \
-        --db_password smailydev1 \
+        --db_host $DB_HOST \
+        --db_user $DB_USER \
+        --db_password $DB_PASSWORD \
         --db_name opencart \
         --db_port 3306 \
         --username admin \
         --password smailydev1 \
-        --email testing@smaily.sandbox \
+        --email admin@smaily.sandbox \
         --agree_tnc yes \
         --http_server http://127.0.0.1:8080/
 
     # Remove install directory.
     rm -rd ./install
-
-    echo 'OpenCart installed!'
 fi
 
 # Link module files to OpenCart installation.
 if [ ! -d ./.modman ]; then
-    modman init
+    su www-data -s /bin/bash -c "modman init"
 fi
-modman link /var/www/html/smaily_for_opencart
+su www-data -s /bin/bash -c "modman link /smaily_for_opencart"
 
 docker-php-entrypoint "$@"
