0b0313351ed2276a591b68bb068c88256feed0ae
TomAbel <tomkristianabel@gmail.com>
2020-11-20 11:32:14 +0200

    Update plugin version number

--- upload/admin/controller/module/smaily_for_opencart.php
+++ upload/admin/controller/module/smaily_for_opencart.php
@@ -11,7 +11,7 @@
  *
  * Plugin Name: Smaily for OpenCart
  * Description: Smaily email marketing and automation extension plugin for OpenCart.
- * Version: 1.1.6
+ * Version: 1.2.0
  * License: GPL3
  * Author: Smaily
  * Author URI: https://smaily.com/
