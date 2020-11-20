183413b1e735659da50e68334d2689feafa65eda
Kait Todesk <kaittodesk@gmail.com>
2020-11-20 11:22:08 +0200

    Simplify development environment management
    With these changes you only need to modify `docker-compose.yaml` file
    to apply changes to database host, username and password.

--- .sandbox/entrypoint.sh
+++ .sandbox/entrypoint.sh
@@ -2,7 +2,7 @@
 
 # Wait for MySQL to start.
 mysql_ready() {
-    mysqladmin ping --host=database --user=root --password=smailydev1 > /dev/null 2>&1
+    mysqladmin ping --host=$DB_HOST --user=$DB_USER --password=$DB_PASSWORD > /dev/null 2>&1
 }
 while !(mysql_ready); do
     sleep 1
@@ -13,28 +13,26 @@ done
 if [ -d install ]; then
     # Install OpenCart through the CLI installer.
     php ./install/cli_install.php install \
-        --db_hostname database \
-        --db_username root \
-        --db_password smailydev1 \
+        --db_hostname $DB_HOST \
+        --db_username $DB_USER \
+        --db_password $DB_PASSWORD \
         --db_database opencart \
         --db_port 3306 \
         --db_driver mysqli \
         --username admin \
         --password smailydev1 \
-        --email testing@smaily.sandbox \
+        --email admin@smaily.sandbox \
         --agree_tnc yes \
-        --http_server http://127.0.0.1:8080/
+        --http_server http://localhost:8080/
 
     # Remove install directory.
     rm -rd ./install
-
-    echo 'OpenCart installed!'
 fi
 
 # Link module files to OpenCart installation.
 if [ ! -d ./.modman ]; then
-    modman init
+    su www-data -s /bin/bash -c "modman init"
 fi
-modman link /var/www/html/smaily_for_opencart
+su www-data -s /bin/bash -c "modman link /smaily_for_opencart"
 
 docker-php-entrypoint "$@"
