a8e0d8b789117c0a86d144ed14b2fa92642cb885
TomAbel <tomkristianabel@gmail.com>
2020-12-09 16:58:26 +0200

    Remove apiCall in favor of request.php

--- upload/admin/model/extension/smailyforopencart/admin.php
+++ upload/admin/model/extension/smailyforopencart/admin.php
@@ -30,65 +30,6 @@ class ModelExtensionSmailyForOpencartAdmin extends Model {
         $this->model_setting_setting->deleteSetting('smaily_for_opencart');
     }
 
-    /**
-     * Makes cUrl call to smaily api endpoint. Returns body if cURL successful, empty if not.
-     *
-     * @param string $endpoint Smaily api endpoint without .php
-     * @param array $data      Data to send to smaily.
-     * @param string $method   POST or GET
-     * @return array $response Response body on successful cURL request, empty otherwise.
-     */
-    public function apiCall($endpoint, array $data = [], $method = 'GET') {
-        // Response.
-        $response = [];
-        // Smaily settings from database.
-        $this->load->model('setting/setting');
-        $settings = $this->model_setting_setting->getSetting('module_smaily_for_opencart');
-        // Credentials
-        $username = $settings['module_smaily_for_opencart_username'];
-        $password = $settings['module_smaily_for_opencart_password'];
-        $subdomain = $settings['module_smaily_for_opencart_subdomain'];
-        // Url.
-        $apiUrl = 'https://' . $subdomain . '.sendsmaily.net/api/' . $endpoint . '.php';
-        // Curl call.
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $apiUrl);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($ch, CURLOPT_USERPWD, "$username:$password");
-
-        // API call with GET request.
-        if ($method === 'GET') {
-            if (!empty($data)) {
-                curl_setopt($ch, CURLOPT_URL, $apiUrl . '?' . http_build_query($data));
-            }
-            $api_call = json_decode(curl_exec($ch), true);
-            // Response code from Smaily API.
-            $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-        // If Method POST.
-        } elseif ($method === 'POST') {
-            curl_setopt($ch, CURLOPT_POST, 1);
-            curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-            $api_call = json_decode(curl_exec($ch), true);
-            // Response code from Smaily API.
-            $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-            // Validate response
-            if (!array_key_exists('code', $api_call)) {
-                throw new SmailyAPIError($response['message'], $response['code']);
-            }
-        }
-        // Return response if success.
-        if ($http_code === 200) {
-            $response = $api_call;
-        } else {
-            // Log error.
-            $this->log->write('Error in smaily api call with code: ' . $http_code);
-        }
-        // Response from API call.
-        return $response;
-    }
-
     public function getAutoresponders() {
         // Smaily settings from database.
         $this->load->model('setting/setting');
