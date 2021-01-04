251e9cebf709babbc65749d78cb6492dc8d8e4ff
TomAbel <tomkristianabel@gmail.com>
2021-01-04 20:07:46 +0200

    Bump version for 1.5.1

--- upload/admin/controller/module/smaily_for_opencart.php
+++ upload/admin/controller/module/smaily_for_opencart.php
@@ -11,7 +11,7 @@
  *
  * Plugin Name: Smaily for OpenCart
  * Description: Smaily email marketing and automation extension plugin for OpenCart.
- * Version: 1.5.0
+ * Version: 1.5.1
  * License: GPL3
  * Author: Smaily
  * Author URI: https://smaily.com/
@@ -32,7 +32,7 @@
 require_once(DIR_SYSTEM . 'library/smailyforopencart/request.php');
 class ControllerModuleSmailyForOpencart extends Controller {
     private $error = array();
-    private $version = '1.5.0';
+    private $version = '1.5.1';
 
     public function index() {
         // Add language file.
