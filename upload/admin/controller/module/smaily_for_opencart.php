99f9dbec6b514f8334c34a787481b1c883801f7c
Tom Abel <tomkristianabel@gmail.com>
2021-01-27 14:51:13 +0200

    RSS product limit expr requires paranthesis

--- upload/admin/controller/module/smaily_for_opencart.php
+++ upload/admin/controller/module/smaily_for_opencart.php
@@ -548,9 +548,9 @@ class ControllerModuleSmailyForOpencart extends Controller {
         }
         // Validate RSS product limit value
         if (isset($this->request->post['smaily_for_opencart_rss_limit'])
-        && (int) $this->request->post['smaily_for_opencart_rss_limit'] < 1
+        && ((int) $this->request->post['smaily_for_opencart_rss_limit'] < 1
         || (int) $this->request->post['smaily_for_opencart_rss_limit'] > 250
-        ) {
+        )) {
             $this->error['rss_limit'] = $this->language->get('rss_limit_error');
         }
         return !$this->error;
